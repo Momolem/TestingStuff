0ce7074b252a9e5eaf1a00fdd0bc6bba9b3f4043
Moritz <skyblock@gmx.de>
2017-09-22 23:45:07 +0200

    Added Infinite Loop

--- Testing Stuff/Program.cs
+++ Testing Stuff/Program.cs
@@ -7,30 +7,32 @@ using System.Threading.Tasks;
 namespace Testing_Stuff {
     class Program {
         static void Main(string[] args) {
-            //Higher lower game
+            //Higher lower 
             bool richtig = false;
+
             Random rnd = new Random();
-
-            int solution = rnd.Next(1, 60);
-
-            while(!richtig)
-            {
-                int antwort = Int32.Parse(Console.In.ReadLine());
-
-                if(antwort > solution)
+            while(true) {
+                int solution = rnd.Next(1, 60);
+                Console.Out.Write("Errate die Zahl");
+                richtig = false;
+                while (!richtig)
                 {
-                    Console.Out.WriteLine("Zu hoch!");
-                } else if(antwort < solution)
-                {
-                    Console.Out.WriteLine("Zu niedrig!");
-                } else
-                {
-                    Console.Out.WriteLine("Richtig!");
-                    richtig = true;
+                    int antwort = Int32.Parse(Console.In.ReadLine());
+
+                    if(antwort > solution)
+                    {
+                        Console.Out.WriteLine("Zu hoch!");
+                    } else if(antwort < solution)
+                    {
+                        Console.Out.WriteLine("Zu niedrig!");
+                    } else
+                    {
+                        Console.Out.WriteLine("Richtig!");
+                        richtig = true;
+                    }
+
                 }
-
             }
-
         }
     }
 }
